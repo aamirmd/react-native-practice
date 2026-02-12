b8421cc6b23beb0fea0bdafa265276bbf2af0520
aamirmd <aamirmd105@gmail.com>
2026-02-12 13:48:42 -0500

    Modals

--- app/index.tsx
+++ app/index.tsx
@@ -1,27 +1,43 @@
 import { useState } from "react";
-import { Button, Image, Pressable, Text, View } from "react-native";
-const logoImg = require('@/assets/images/icon.png')
+import { Button, Image, Modal, Text, View } from "react-native";
+const logoImg = require("@/assets/images/icon.png");
 
 export default function Index() {
-  const [message, setMessage] = useState("Message");
-  return (
-    <View
-      style={{
-        flex: 1,
-        alignItems: "center",
-        justifyContent: "center"
-      }}
-    >
-      <Pressable 
-        onPress={() => console.log("Image pressed")}
-        onPressIn={() => console.log("On press in")}
-        onPressOut={() => console.log("On press out")}
-        onLongPress={() => console.log("On Long press")}
-      >
-      <Image source={logoImg} style={{width: 300, height: 300}}/>
-      </Pressable>
-      <Text style={{margin: 30}}>{message}</Text>
-      <Button title="Click Me" color="#f704ae" onPress={() => console.log("Hello")} disabled={false}/>
-    </View>
-  );
+    const [message, setMessage] = useState("Message");
+    const [modalVisible, setModalVisible] = useState(false);
+
+    return (
+        <View
+            style={{
+                flex: 1,
+                alignItems: "center",
+                justifyContent: "center",
+            }}
+        >
+            <Modal
+                visible={modalVisible}
+                onRequestClose={() => setModalVisible(false)}
+                animationType="fade"
+                presentationStyle="formSheet"
+            >
+                <View style={{ padding: 30 }}>
+                    <Image
+                        source={logoImg}
+                        style={{ width: 300, height: 300 }}
+                    />
+                    <Button
+                        title="Go back"
+                        onPress={() => setModalVisible(!modalVisible)}
+                    />
+                </View>
+            </Modal>
+            <Text style={{ margin: 30 }}>{message}</Text>
+            <Button
+                title="Click Me"
+                color="#f704ae"
+                onPress={() => setModalVisible(!modalVisible)}
+                disabled={false}
+            />
+        </View>
+    );
 }
