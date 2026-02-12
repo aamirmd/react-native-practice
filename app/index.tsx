9689ed7f1ea530159acecba0cca3273412527de4
aamirmd <aamirmd105@gmail.com>
2026-02-11 19:40:42 -0500

    Image and Image Background practice

--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,5 @@
-import { Text, View } from "react-native";
+import { Image, ImageBackground, Text, View } from "react-native";
+const logoImg = require('@/assets/images/icon.png')
 
 export default function Index() {
   return (
@@ -9,8 +10,12 @@ export default function Index() {
         alignItems: "center",
       }}
     >
+      <Text style={{color: "cyan"}}>Hello World!</Text>
       <Text>Hello World!</Text>
-      <Text>Hello World!</Text>
+      <Image source={logoImg} style={{ width: 200, height: 200 }}/>
+      <ImageBackground source={{uri: "https://www.followingislam.org/_next/image?url=%2Fimages%2Ffuneral-home.jpeg&w=1920&q=75"}} style={{ width: 200, height: 200, margin: 20 }}>
+        <Text>IMAGE TEXT</Text>
+      </ImageBackground>
     </View>
   );
 }
