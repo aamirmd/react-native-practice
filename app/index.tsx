ad5c159355fa9fa3fa74903704756aa66561aa63
aamirmd <aamirmd105@gmail.com>
2026-02-12 13:18:37 -0500

    Pressable

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Button, Text, View } from "react-native";
+import { Button, Image, Pressable, Text, View } from "react-native";
 const logoImg = require('@/assets/images/icon.png')
 
 export default function Index() {
@@ -12,6 +12,14 @@ export default function Index() {
         justifyContent: "center"
       }}
     >
+      <Pressable 
+        onPress={() => console.log("Image pressed")}
+        onPressIn={() => console.log("On press in")}
+        onPressOut={() => console.log("On press out")}
+        onLongPress={() => console.log("On Long press")}
+      >
+      <Image source={logoImg} style={{width: 300, height: 300}}/>
+      </Pressable>
       <Text style={{margin: 30}}>{message}</Text>
       <Button title="Click Me" color="#f704ae" onPress={() => console.log("Hello")} disabled={false}/>
     </View>
