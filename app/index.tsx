85e6879f3167ebc647f5bb313c76b79d33a76684
aamirmd <aamirmd105@gmail.com>
2026-02-19 13:16:54 -0500

    Status bar

--- app/index.tsx
+++ app/index.tsx
@@ -1,42 +1,19 @@
-import { useState } from "react";
-import { Button, Image, Modal, Text, View } from "react-native";
+import { StatusBar, View } from "react-native";
 const logoImg = require("@/assets/images/icon.png");
 
 export default function Index() {
-    const [message, setMessage] = useState("Message");
-    const [modalVisible, setModalVisible] = useState(false);
-
     return (
         <View
             style={{
                 flex: 1,
-                alignItems: "center",
-                justifyContent: "center",
+                padding: 60,
+                backgroundColor: "plum",
             }}
         >
-            <Modal
-                visible={modalVisible}
-                onRequestClose={() => setModalVisible(false)}
-                animationType="fade"
-                presentationStyle="formSheet"
-            >
-                <View style={{ padding: 30 }}>
-                    <Image
-                        source={logoImg}
-                        style={{ width: 300, height: 300 }}
-                    />
-                    <Button
-                        title="Go back"
-                        onPress={() => setModalVisible(!modalVisible)}
-                    />
-                </View>
-            </Modal>
-            <Text style={{ margin: 30 }}>{message}</Text>
-            <Button
-                title="Click Me"
-                color="#f704ae"
-                onPress={() => setModalVisible(!modalVisible)}
-                disabled={false}
+            <StatusBar
+                barStyle="dark-content"
+                backgroundColor="lightgreen"
+                hidden={false}
             />
         </View>
     );
