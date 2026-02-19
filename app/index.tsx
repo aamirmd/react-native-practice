4e3b81c118c1ee53c5efb1f451c9cd368542d71d
aamirmd <aamirmd105@gmail.com>
2026-02-19 15:27:13 -0500

    Stylesheet API

--- app/index.tsx
+++ app/index.tsx
@@ -1,18 +1,22 @@
-import { Greet } from "@/components/Greet";
-import { View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 // const logoImg = require("@/assets/images/icon.png");
 
 export default function Index() {
     return (
-        <View
-            style={{
-                flex: 1,
-                padding: 60,
-                backgroundColor: "plum",
-            }}
-        >
-            <Greet name="Ahmed" />
-            <Greet name="Abdullah" />
+        <View style={styles.container}>
+            <Text style={styles.text}>Some Text</Text>
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        padding: 60,
+        backgroundColor: "cyan",
+    },
+
+    text: {
+        color: "green",
+    },
+});
