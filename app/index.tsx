80b91b6676ad14096b57675aec70186521437a9f
aamirmd <aamirmd105@gmail.com>
2026-05-21 22:59:33 -0400

    Justify content property

--- app/index.tsx
+++ app/index.tsx
@@ -7,10 +7,10 @@ export default function Index() {
             <Box style={{ backgroundColor: "#8e9b00" }}>Box 1</Box>
             <Box style={{ backgroundColor: "#2a9d8f" }}>Box 2</Box>
             <Box style={{ backgroundColor: "#e9c46a" }}>Box 3</Box>
-            <Box style={{ backgroundColor: "#f4a261" }}>Box 4</Box>
+            {/* <Box style={{ backgroundColor: "#f4a261" }}>Box 4</Box>
             <Box style={{ backgroundColor: "#e76f51" }}>Box 5</Box>
             <Box style={{ backgroundColor: "#457b9d" }}>Box 6</Box>
-            <Box style={{ backgroundColor: "#a8dadc" }}>Box 7</Box>
+            <Box style={{ backgroundColor: "#a8dadc" }}>Box 7</Box> */}
         </View>
     );
 }
@@ -18,10 +18,13 @@ export default function Index() {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        // flexDirection: "column-reverse",
-        // flexDirection: "column",
-        // flexDirection: "row-reverse",
         flexDirection: "row",
+        // justifyContent: "space-evenly",
+        // justifyContent: "flex-start",
+        // justifyContent: "flex-end",
+        justifyContent: "space-around",
+        // justifyContent: "space-between",
+        // justifyContent: "center",
         marginTop: 64,
         borderWidth: 6,
         borderColor: "red",
