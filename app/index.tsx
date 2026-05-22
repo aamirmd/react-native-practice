843f75fa165a3c52922fe8936c8ad70626bbbce3
aamirmd <aamirmd105@gmail.com>
2026-05-21 22:07:23 -0400

    Fully set up for learning flex box

--- app/index.tsx
+++ app/index.tsx
@@ -4,20 +4,20 @@ import { StyleSheet, View } from "react-native";
 export default function Index() {
     return (
         <View style={styles.container}>
-            <Box style={{ backgroundColor: "8e9b00" }}>Box 1</Box>
-            <Box style={{ backgroundColor: "8e9b00" }}>Box 2</Box>
-            <Box style={{ backgroundColor: "8e9b00" }}>Box 3</Box>
-            <Box style={{ backgroundColor: "8e9b00" }}>Box 4</Box>
-            <Box style={{ backgroundColor: "8e9b00" }}>Box 5</Box>
-            <Box style={{ backgroundColor: "8e9b00" }}>Box 6</Box>
-            <Box style={{ backgroundColor: "8e9b00" }}>Box 7</Box>
+            <Box style={{ backgroundColor: "#8e9b00" }}>Box 1</Box>
+            <Box style={{ backgroundColor: "#2a9d8f" }}>Box 2</Box>
+            <Box style={{ backgroundColor: "#e9c46a" }}>Box 3</Box>
+            <Box style={{ backgroundColor: "#f4a261" }}>Box 4</Box>
+            <Box style={{ backgroundColor: "#e76f51" }}>Box 5</Box>
+            <Box style={{ backgroundColor: "#457b9d" }}>Box 6</Box>
+            <Box style={{ backgroundColor: "#a8dadc" }}>Box 7</Box>
         </View>
     );
 }
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
+        marginTop: 64,
         borderWidth: 6,
         borderColor: "red",
     },
