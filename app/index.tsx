88595dd50366e931928970a30ee8eada90312f26
aamirmd <aamirmd105@gmail.com>
2026-05-21 23:05:06 -0400

    Align items property

--- app/index.tsx
+++ app/index.tsx
@@ -5,12 +5,14 @@ export default function Index() {
     return (
         <View style={styles.container}>
             <Box style={{ backgroundColor: "#8e9b00" }}>Box 1</Box>
-            <Box style={{ backgroundColor: "#2a9d8f" }}>Box 2</Box>
+            <Box style={{ backgroundColor: "#2a9d8f", paddingVertical: 79 }}>
+                Box 2
+            </Box>
             <Box style={{ backgroundColor: "#e9c46a" }}>Box 3</Box>
-            {/* <Box style={{ backgroundColor: "#f4a261" }}>Box 4</Box>
+            <Box style={{ backgroundColor: "#f4a261" }}>Box 4</Box>
             <Box style={{ backgroundColor: "#e76f51" }}>Box 5</Box>
             <Box style={{ backgroundColor: "#457b9d" }}>Box 6</Box>
-            <Box style={{ backgroundColor: "#a8dadc" }}>Box 7</Box> */}
+            <Box style={{ backgroundColor: "#a8dadc" }}>Box 7</Box>
         </View>
     );
 }
@@ -19,12 +21,7 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         flexDirection: "row",
-        // justifyContent: "space-evenly",
-        // justifyContent: "flex-start",
-        // justifyContent: "flex-end",
-        justifyContent: "space-around",
-        // justifyContent: "space-between",
-        // justifyContent: "center",
+        alignItems: "baseline",
         marginTop: 64,
         borderWidth: 6,
         borderColor: "red",
