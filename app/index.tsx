1c3131960979cf2f5efa034a0861dc0860b97174
aamirmd <aamirmd105@gmail.com>
2026-05-22 20:17:02 -0400

    flexShrink

--- app/index.tsx
+++ app/index.tsx
@@ -4,20 +4,18 @@ import { StyleSheet, View } from "react-native";
 export default function Index() {
     return (
         <View style={styles.container}>
-            <Box style={{ backgroundColor: "#8e9b00" }}>Box 1</Box>
-            {/* Available space is distributed based on flex basis property instead of height property (or width depending on main axis) */}
-            <Box
-                style={{ backgroundColor: "#2a9d8f", flexBasis: 140, flex: 1 }}
-            >
+            <Box style={{ backgroundColor: "#8e9b00", flexShrink: 1 }}>
+                Box 1
+            </Box>
+            <Box style={{ backgroundColor: "#2a9d8f", flexShrink: 3 }}>
                 Box 2
             </Box>
-            <Box style={{ backgroundColor: "#e9c46a", height: 140, flex: 1 }}>
-                Box 3
-            </Box>
+            {/* Used to shrink items to fit inside container */}
+            {/* <Box style={{ backgroundColor: "#e9c46a" }}>Box 3 </Box>
             <Box style={{ backgroundColor: "#f4a261" }}>Box 4</Box>
             <Box style={{ backgroundColor: "#e76f51" }}>Box 5</Box>
             <Box style={{ backgroundColor: "#457b9d" }}>Box 6</Box>
-            <Box style={{ backgroundColor: "#a8dadc" }}>Box 7</Box>
+            <Box style={{ backgroundColor: "#a8dadc" }}>Box 7</Box> */}
         </View>
     );
 }
@@ -26,6 +24,9 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         marginTop: 64,
+        width: 180,
+        flexDirection: "row",
+        alignItems: "flex-start",
         borderWidth: 6,
         borderColor: "red",
     },
