2418692dc29519ba623344cbce14125766f77579
aamirmd <aamirmd105@gmail.com>
2026-05-22 20:28:35 -0400

    flexGrow

--- app/index.tsx
+++ app/index.tsx
@@ -4,18 +4,14 @@ import { StyleSheet, View } from "react-native";
 export default function Index() {
     return (
         <View style={styles.container}>
-            <Box style={{ backgroundColor: "#8e9b00", flexShrink: 1 }}>
-                Box 1
-            </Box>
-            <Box style={{ backgroundColor: "#2a9d8f", flexShrink: 3 }}>
-                Box 2
-            </Box>
-            {/* Used to shrink items to fit inside container */}
-            {/* <Box style={{ backgroundColor: "#e9c46a" }}>Box 3 </Box>
+            <Box style={{ backgroundColor: "#8e9b00" }}>Box 1</Box>
+            <Box style={{ backgroundColor: "#2a9d8f" }}>Box 2</Box>
+            <Box style={{ backgroundColor: "#e9c46a" }}>Box 3 </Box>
             <Box style={{ backgroundColor: "#f4a261" }}>Box 4</Box>
-            <Box style={{ backgroundColor: "#e76f51" }}>Box 5</Box>
-            <Box style={{ backgroundColor: "#457b9d" }}>Box 6</Box>
-            <Box style={{ backgroundColor: "#a8dadc" }}>Box 7</Box> */}
+            {/* Used for individual items to grow to take up extra space in parent container. Is a wrapper around 'flex'  */}
+            <Box style={{ backgroundColor: "#e76f51", flexGrow: 1 }}>Box 5</Box>
+            <Box style={{ backgroundColor: "#457b9d", flexGrow: 3 }}>Box 6</Box>
+            <Box style={{ backgroundColor: "#a8dadc" }}>Box 7</Box>
         </View>
     );
 }
@@ -24,9 +20,6 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         marginTop: 64,
-        width: 180,
-        flexDirection: "row",
-        alignItems: "flex-start",
         borderWidth: 6,
         borderColor: "red",
     },
