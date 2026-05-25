0c53f7789ea22db8fb6d579e60eb19365cf4a102
aamirmd <aamirmd105@gmail.com>
2026-05-24 22:21:41 -0400

    Safe Area View

--- app/index.tsx
+++ app/index.tsx
@@ -1,38 +1,35 @@
-import { StyleSheet, Text, View, useWindowDimensions } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
-    const windowWidth = useWindowDimensions().width;
-    const windowHeight = useWindowDimensions().height;
-
     return (
-        <View style={styles.container}>
-            <View
-                style={[
-                    styles.box,
-                    {
-                        width: windowWidth > 500 ? "70%" : "90%",
-                        height: windowHeight > 600 ? "60%" : "90%",
-                    },
-                ]}
-            >
-                <Text style={{ fontSize: windowWidth > 500 ? 50 : 24 }}>
-                    Welcome!
-                </Text>
+        <SafeAreaView style={styles.safeContainer}>
+            <View style={styles.container}>
+                <View style={styles.box}>
+                    <Text style={styles.text}>Welcome!</Text>
+                </View>
             </View>
-        </View>
+        </SafeAreaView>
     );
 }
 
 const styles = StyleSheet.create({
-    container: {
+    safeContainer: {
         flex: 1,
-        alignItems: "center",
-        justifyContent: "center",
         backgroundColor: "plum",
     },
+    container: {
+        flex: 1,
+        backgroundColor: "plum",
+        borderColor: "red",
+        borderWidth: 4,
+    },
     box: {
-        backgroundColor: "lightblue",
-        alignItems: "center",
-        justifyContent: "center",
+        padding: 0,
+    },
+    text: {
+        fontSize: 24,
+        fontWeight: "bold",
+        textAlign: "center",
     },
 });
