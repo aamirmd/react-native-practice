0840c89a7b00387881d34eab76ef011637ea7f1f
aamirmd <aamirmd105@gmail.com>
2026-05-24 20:21:41 -0400

    Dimensions API

--- app/index.tsx
+++ app/index.tsx
@@ -1,36 +1,33 @@
-import Box from "@/components/Box";
-import { StyleSheet, View } from "react-native";
+import { Dimensions, StyleSheet, Text, View } from "react-native";
 
 export default function Index() {
     return (
         <View style={styles.container}>
-            <Box style={{ backgroundColor: "#8e9b00", left: 200, top: 50 }}>
-                Box 1
-            </Box>
-            <Box style={{ backgroundColor: "#2a9d8f" }}>Box 2</Box>
-            <Box style={{ backgroundColor: "#e9c46a" }}>Box 3 </Box>
-            <Box
-                style={{
-                    backgroundColor: "#f4a261",
-                    position: "absolute",
-                    top: 100,
-                    left: 100,
-                }}
-            >
-                Box 4
-            </Box>
-            <Box style={{ backgroundColor: "#e76f51" }}>Box 5</Box>
-            <Box style={{ backgroundColor: "#457b9d" }}>Box 6</Box>
-            <Box style={{ backgroundColor: "#a8dadc" }}>Box 7</Box>
+            <View style={styles.box}>
+                <Text style={styles.text}>Welcome!</Text>
+            </View>
         </View>
     );
 }
 
+const windowHeight = Dimensions.get("window").height;
+const windowWidth = Dimensions.get("window").width;
+
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        marginTop: 16,
-        borderWidth: 6,
-        borderColor: "red",
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: "plum",
+    },
+    box: {
+        width: windowWidth > 500 ? "70%" : "90%",
+        height: windowHeight > 600 ? "60%" : "90%",
+        backgroundColor: "lightblue",
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    text: {
+        fontSize: windowWidth > 500 ? 50 : 24,
     },
 });
