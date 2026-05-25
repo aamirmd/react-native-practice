57a26a8268d8d9f724df60ac5b1f424b5affbee9
aamirmd <aamirmd105@gmail.com>
2026-05-24 20:37:07 -0400

    useWindowDimensions

--- app/index.tsx
+++ app/index.tsx
@@ -1,17 +1,27 @@
-import { Dimensions, StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, useWindowDimensions } from "react-native";
 
 export default function Index() {
+    const windowWidth = useWindowDimensions().width;
+    const windowHeight = useWindowDimensions().height;
+
     return (
         <View style={styles.container}>
-            <View style={styles.box}>
-                <Text style={styles.text}>Welcome!</Text>
+            <View
+                style={[
+                    styles.box,
+                    {
+                        width: windowWidth > 500 ? "70%" : "90%",
+                        height: windowHeight > 600 ? "60%" : "90%",
+                    },
+                ]}
+            >
+                <Text style={{ fontSize: windowWidth > 500 ? 50 : 24 }}>
+                    Welcome!
+                </Text>
             </View>
         </View>
     );
 }
-
-const windowHeight = Dimensions.get("window").height;
-const windowWidth = Dimensions.get("window").width;
 
 const styles = StyleSheet.create({
     container: {
@@ -21,13 +31,8 @@ const styles = StyleSheet.create({
         backgroundColor: "plum",
     },
     box: {
-        width: windowWidth > 500 ? "70%" : "90%",
-        height: windowHeight > 600 ? "60%" : "90%",
         backgroundColor: "lightblue",
         alignItems: "center",
         justifyContent: "center",
-    },
-    text: {
-        fontSize: windowWidth > 500 ? 50 : 24,
     },
 });
